1a5e792618d44ee2e9aba742c300d6618bf3798c
Prishantina <prishantina@gmail.com>
2022-01-22 18:35:39 -0300

    navigation

---
+++ src/Components/NavBar.js
@@ -0,0 +1,15 @@
+import "./NavBar.css";
+const NavBar = () => {
+    return ( 
+        <nav className="Navigation">
+            <ul>
+                <li>Home</li>
+                <li>Productos</li>
+                <li>Nosotros</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>   
+    ) 
+};
+
+export default NavBar;
