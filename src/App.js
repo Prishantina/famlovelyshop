f0aeac50c3bb19b99a59083c86f02219b2ece908
Prishantina <prishantina@gmail.com>
2022-01-26 08:27:58 -0300

    agrego cards y props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import NavBar from './Components/NavBar.js';
+import imagen from './Components/logo-fam/logo.png';
+import CardComponent from './Components/CardComponent/CardComponent';
+
+const greyColor = "grey";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+{        <img src={imagen} alt='logo-fam' width="220px" height="auto" /> }
           <NavBar/>
+          <CardComponent color= {greyColor} />
+          <CardComponent color= {greyColor} />
+          <CardComponent color= {greyColor} />
       </header>
     </div>
   );
---
+++ src/Components/CardComponent/CardComponent.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './CardComponent.css';
+import { Card, CardBody, CardSubtitle, CardText, CardTitle, Button } from 'reactstrap';
+
+
+const CardComponent = (props) => {
+    return (
+<div>
+  <Card className="Products">
+    <CardBody style={{ border: `10px solid ${props.color}` }}>
+      <CardTitle tag="h5">
+        Card title
+      </CardTitle>
+      <CardSubtitle
+        className="mb-2 text-muted"
+        tag="h6"
+      >
+        Card subtitle
+      </CardSubtitle>
+      <CardText>
+        Some quick example text to build on the card title and make up the bulk of the card's content.
+      </CardText>
+      <Button>
+        +Agregar al carrito
+      </Button>
+    </CardBody>
+  </Card>
+</div>
+    )
+};
+ export default CardComponent
